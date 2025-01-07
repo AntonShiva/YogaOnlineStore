e96e4b8d6b57d04e934ddca3c7b31d499056e768
AntonShiva <ryashinanthon@yandex.ru>
2025-01-07 10:49:05 +0300

    Initial Commit

---
+++ YogaOnlineStore/YogaOnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogaOnlineStoreApp.swift
+//  YogaOnlineStore
+//
+//  Created by Anton Rasen on 07.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct YogaOnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
