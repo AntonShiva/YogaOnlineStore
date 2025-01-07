0406a16cc718a8393c92065c98fcc42c7488e6b1
AntonShiva <ryashinanthon@yandex.ru>
2025-01-07 19:27:37 +0300

    Создал модель тоовара, создал экземпляр товара в Firebase и отобразил его на главном экране

--- YogaOnlineStore/ContentView.swift
+++ YogaOnlineStore/ContentView.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct ContentView: View {
+    @FirestoreQuery(collectionPath: "YogaShop") var items: [UnitOfGoods]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List{
+                ForEach(items) {item in
+                    Text(item.nameOfGoods)
+                    Image(item.image)
+                        .resizable()
+                        .frame(height: 200)
+                    Text("Цена \(item.price) руб.")
+                    Text(item.goodsDescription)
+                }
+            }
         }
         .padding()
     }
---
+++ YogaOnlineStore/UnitOfGoods.swift
@@ -0,0 +1,22 @@
+//
+//  UnitOfGoods.swift
+//  YogaOnlineStore
+//
+//  Created by Anton Rasen on 07.01.2025.
+//
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+struct UnitOfGoods: Codable, Identifiable {
+    @DocumentID var id: String?
+    var nameOfGoods: String
+    var goodsDescription: String
+    var image: String
+    var quantity: Int?
+    var price: Int
+    var isFavoriteProduct: Bool
+
+}
+
+
